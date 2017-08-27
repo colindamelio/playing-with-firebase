b101e02bf9b499a6a567e79bbe49cc8cec79cc37
colindamelio <colin.damelio@gmail.com>
2017-08-27 14:09:14 -0400

    loading

--- src/App.js
+++ src/App.js
@@ -5,101 +5,97 @@ import Panel from './components/panel.js';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super()
+	constructor() {
+		super();
 
-    this.state = {
-      artist: '',
-      album: '',
-      song: '',
-      music: [],
-    }
+		this.state = {
+			loading: true,
+			artist: '',
+			album: '',
+			song: '',
+			music: []
+		};
 
-    this.handleFormChange = this.handleFormChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    this.removeItem = this.removeItem.bind(this);
+		this.handleFormChange = this.handleFormChange.bind(this);
+		this.handleFormSubmit = this.handleFormSubmit.bind(this);
+		this.removeItem = this.removeItem.bind(this);
+	}
 
+	componentDidMount() {
+    this.loadingState();
+		this.handleDataRetreival();
+	}
+
+  loadingState() {
+    setTimeout(() => this.setState({ loading: false }), 1000);
   }
 
-  componentDidMount() {
-    this.handleDataRetreival();
-  }
+	handleDataRetreival() {
+		// takes snapshot of what currently exists in the DB
+		const itemsRef = firebase.database().ref('items');
+		itemsRef.on('value', snapshopt => {
+			let items = snapshopt.val();
+			let updatedState = [];
 
-  handleDataRetreival() {
-    // takes snapshot of what currently exists in the DB
-    const itemsRef = firebase.database().ref('items');
-    itemsRef.on('value', (snapshopt) => {
-      let items = snapshopt.val();
-      let updatedState = [];
+			for (let item in items) {
+				updatedState.push({
+					artist: items[item].artist,
+					album: items[item].album,
+					song: items[item].song,
+					id: item
+				});
+			}
 
-      for (let item in items) {
-        updatedState.push({
-          artist: items[item].artist,
-          album: items[item].album,
-          song: items[item].song,
-          id: item,
-        })
-      };
+			this.setState({
+				music: updatedState
+			});
+		});
+	}
 
-      this.setState({
-        music: updatedState,
-      });
-    });
-  };
+	handleFormChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
 
-  handleFormChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    })
-  };
+	handleFormSubmit(e) {
+		e.preventDefault();
+		const itemsRef = firebase.database().ref('items');
+		const item = {
+			artist: this.state.artist,
+			album: this.state.album,
+			song: this.state.song
+		};
 
-  handleFormSubmit(e) {
-    e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    const item = {
-      artist: this.state.artist,
-      album: this.state.album,
-      song: this.state.song,
-    }
+		itemsRef.push(item);
+		this.setState({
+			artist: '',
+			album: '',
+			song: ''
+		});
+	}
 
-    itemsRef.push(item);
-    this.setState({
-      artist: '',
-      album: '',
-      song: '',
-    })
-  };
+	removeItem(itemId) {
+		const itemsRef = firebase.database().ref(`items/${itemId}`);
+		itemsRef.remove();
+	}
 
-  removeItem(itemId) {
-    const itemsRef = firebase.database().ref(`items/${itemId}`);
-    itemsRef.remove();
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <PlayListInput onSubmit={this.handleFormSubmit} onChange={this.handleFormChange} />
-        <div className="panel">
-          <Panel details={this.state.music} removeItem={this.removeItem}/>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const { loading } = this.state;
+		return !loading
+			? <div className="App">
+					<PlayListInput
+						onSubmit={this.handleFormSubmit}
+						onChange={this.handleFormChange}
+					/>
+					<div className="panel">
+						<Panel
+              details={this.state.music}
+              removeItem={this.removeItem} />
+					</div>
+				</div>
+			: null;
+	}
 }
 
 export default App;
-
-/* <div className="panel">
-  {this.state.music.map((item, i) => {
-    return (
-      <div className="panel-details" key={item.id}>
-        <button
-          className="remove"
-          onClick={() => {this.removeItem(item.id)}}>X</button>
-        <span>{item.artist}</span>
-        <span className="album">{item.album}</span>
-        <span>{item.song}</span>
-      </div>
-    )
-  })}
-</div> */
